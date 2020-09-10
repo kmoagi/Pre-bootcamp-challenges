d90ecf8374107ade6715a7d073f1d4e5dc5445ca
kmoagi <33934323+kmoagi@users.noreply.github.com>
2020-09-10 05:21:03 +0200

    Created a Function with if statements

--- Task 1/index.js
+++ Task 1/index.js
@@ -3,20 +3,34 @@
 // hello Umuzi heres my psuedocode solution task 
 /////////// Task One /////////////
 
+
+function myFunction(){
+
 var x = 0;
 var y = 1;
 
-console.log(x);
-console.log(y);
 
-if (x += 3 ){
+if ( x < y){
 
-    console.log(x) ;
+    console.log(x);
+}
+ if (y > x){
+
+    console.log(y);
+
+ }
+
+  if (x = x +3){
+
+    console.log(x)
 }
 
-if  (y += x){
+ if (y = y + x ){
 
-    console.log(y) ;
+    console.log(y);
+}
+
 }
 
 
+myFunction();
