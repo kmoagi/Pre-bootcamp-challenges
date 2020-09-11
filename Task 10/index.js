bd0c0115e4e5301dc8b760884eeee33010caf0f3
kmoagi <33934323+kmoagi@users.noreply.github.com>
2020-09-11 11:22:30 +0200

    created a function

--- Task 10/index.js
+++ Task 10/index.js
@@ -2,14 +2,23 @@
      
      // Task Ten
        
-     var vowels = "aeiouAEIOU";
-     var string = "Hello Umuzi";
-     
-     var array = "";
-     for (i = 0; i < string.length; i++) {
-       if(vowels.indexOf(string[i]) >= 0) array += string[i];
-     }
-     console.log(array);
+    
+      function vowelCheck(string){
 
+        
+      var vowels = "aeiouAEIOU";
+      var string = "Hello Umuzi";
+      var arr = "";
+      
+      
+      for (i = 0; i < string.length; i++) {
+        if(vowels.indexOf(string[i]) >= 0) arr += string[i];
+      }
+      console.log(arr);
+
+      }
+
+
+      vowelCheck();
      
      
