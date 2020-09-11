c930825426f891d67160ccef5bc1a1e44955014d
kmoagi <33934323+kmoagi@users.noreply.github.com>
2020-09-11 11:25:29 +0200

    fixed function / linear operation

--- Task 11/index.js
+++ Task 11/index.js
@@ -3,20 +3,31 @@
       // Task Eleven 
       
 
-     function char_count(str1, letter) 
-      {
-     var letter_Count = 0;
-     for (var position = 0; position < str1.length; position++) 
-      {
-     if (str1.charAt(position) == letter) 
-      {
-     letter_Count += 1;
-      }
-      }
-     return letter_Count;
-      }
+     function commonCharactors(str1,str2){
 
-     console.log(char_count('kabelomg@gmail.com', 'g'));
+      var str1 = "kabelo";
+      var str2 = "karabo";    
+
+      var one = str1.replace(/ /g, "");
+      var two = str2.replace(/ /g, "");
+
+     var result = [];
+
+      for (var i = 0; i < one.length; i++  ){
+
+           if(two.indexOf(one[i]) !== -1 && result.indexOf(one[i]) === -1){
+
+
+            result.push(one[i]);
+           }
+
+      }
+      console.log(result);
+     
+     }
+
+
+ commonCharactors();
 
 
      
