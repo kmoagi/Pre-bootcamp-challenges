c3c2ecd5772b33302db71749eb9a515511b25b56
kmoagi <33934323+kmoagi@users.noreply.github.com>
2020-09-11 11:18:38 +0200

    added strings :hours and minutes

--- Task 8/index.js
+++ Task 8/index.js
@@ -3,19 +3,16 @@
       // Task Eight 
 
 
-    function timeConvert(n) {
-            var num = n;
-            var hours = (num / 60);
-            var rhours = Math.floor(hours);
-            var minutes = (hours - rhours) * 60;
-            var rminutes = Math.round(minutes);
+    function timeConvert(num)
+      { 
             
-            
-            return rhours + " hour(s) and " + rminutes + " minute(s).";
-            }
-            
-            console.log(timeConvert(200));
-
+       
+       var hours = Math.floor(num / 60);  
+       var minutes = num % 60;
+       return  hours + "hours" + "," +  minutes + "minutes" ;         
+     }
+     
+     console.log(timeConvert(133));
 
 
      
